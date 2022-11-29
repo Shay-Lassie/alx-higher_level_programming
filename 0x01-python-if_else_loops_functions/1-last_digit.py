5d31abca898c6c4375b4a18dca7768861036f3e2
Shay-Lassie <lassie034@gmail.com>
2022-11-29 12:27:37 +0000

    was more concerned about code ineffeciency to realise the error

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -11,11 +11,8 @@ if number >= 0:
         print("{} and is 0".format(la))
     else:
         print("{} and is less than 6 and not 0".format(la))
-if number <= 0:
+if number < 0:
     n = number
     la = n % -10
     print("Last digit of {} is ".format(n), end="")
-    if la == 0:
-        print("{} and is 0".format(la))
-    else:
-        print("{} and is less than 6 and not 0".format(la))
+    print("{} and is less than 6 and not 0".format(la))
