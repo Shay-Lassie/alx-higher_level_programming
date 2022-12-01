5dbb2fa57098aab0d6521e9a01022e39c3196e61
Shay-Lassie <lassie034@gmail.com>
2022-12-01 11:34:24 +0000

    print number of and list of arguments

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    """imports functions from calculator_1.py Does some Mathe prints result"""
+    """imports functions from calculator_1.py 
+    Does some Mathe prints result"""
     from calculator_1 import add, sub, mul, div
 
     a = 10
