270fc0e078e72fe7281968a92f15d9b324e2f3a7
Shay-Lassie <lassie034@gmail.com>
2022-12-01 11:25:35 +0000

    typo

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-if __name__ == "___main__":
-    """imports functions from calculator_1.py
-        Does some Mathe
-        prints result"""
-
+if __name__ == "__main__":
+    """imports functions from calculator_1.py Does some Mathe prints result"""
     from calculator_1 import add, sub, mul, div
 
     a = 10
     b = 5
+
     print("{} + {} = {}".format(a, b, add(a, b)))
     print("{} - {} = {}".format(a, b, sub(a, b)))
     print("{} * {} = {}".format(a, b, mul(a, b)))
