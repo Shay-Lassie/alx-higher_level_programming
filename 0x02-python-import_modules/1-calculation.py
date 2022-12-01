6599fbb363e7e0a3a33eaf1fa3817e9e5e7b5da9
Shay-Lassie <lassie034@gmail.com>
2022-12-01 10:06:56 +0000

    was ommitting to run as main

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-"""
-    imports functions from calculator_1.py
-    does some Mathe
-    prints result
-"""
-from calculator_1 import add, sub, mul, div
-a = 10
-b = 5
-print("{} + {} = {}".format(a, b, add(a, b)))
-print("{} - {} = {}".format(a, b, sub(a, b)))
-print("{} * {} = {}".format(a, b, mul(a, b)))
-print("{} / {} = {}".format(a, b, div(a, b)))
+if __name__ == "___main__":
+    """imports functions from calculator_1.py
+        Does some Mathe
+        prints result"""
+
+    from calculator_1 import add, sub, mul, div
+
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
